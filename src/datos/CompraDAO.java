e818814c2058f0bdc9d96b8a62a2584d456d25ec
Luis Piedra <enrique_26_2002@hotmail.com>
2021-10-30 17:49:49 -0500

    supervisorDAO y compraDAO

---
+++ src/datos/CompraDAO.java
@@ -0,0 +1,159 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datos;
+import entidad.Compra;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JOptionPane;
+import java.sql.*;
+/**
+ *
+ * @author Usuario
+ */
+public class CompraDAO {
+    private Connection cn = null;
+    private ResultSet rs = null;
+    
+    private static CompraDAO instancia;
+    
+    public static CompraDAO getInstancia(){
+        if(instancia == null)
+            instancia = new CompraDAO();
+        return instancia;
+    }
+    public void agregar(Compra compra) throws SQLException{
+        cn = Conexion.getInstancia().miConexion();
+        PreparedStatement ps = null;
+        
+        try {
+            String idCompra = compra.getIdCompra();
+            double importe = compra.getImporte();
+            String fechaHora = compra.getFechaHora();
+            String sql = "insert into supermercado(idCompra, importe, fechaHora) values(?, ?, ?)";
+            ps = cn.prepareStatement(sql);
+            ps.setString(1, idCompra);
+            ps.setDouble(2, importe);
+            ps.setString(3, fechaHora);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+        }finally{
+            cn.close();
+            ps.close();
+        }
+    }
+    
+    public Compra buscarCompra(String idCompra) throws SQLException{
+        cn = Conexion.getInstancia().miConexion();
+        PreparedStatement ps = null;
+        Compra compra = null;
+        try {
+            String sql = "select * from compra where idCompra = ?";
+            ps = cn.prepareStatement(sql);
+            ps.setString(1, idCompra);
+            rs = ps.executeQuery();
+            if(rs.next()){
+                double importe = rs.getDouble("importe");
+                String fechaHora = rs.getString("fechaHora");
+                compra = new Compra(idCompra, importe, fechaHora);
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+        }finally{
+            cn.close();
+            ps.close();
+        }
+        return compra;
+    }
+    
+    public void actualizar(Compra compra) throws SQLException{
+        cn = Conexion.getInstancia().miConexion();
+        PreparedStatement ps = null;
+        try {
+            String idCompra = compra.getIdCompra();
+            double importe = compra.getImporte();
+            String fechaHora = compra.getFechaHora();
+            String sql = "update compra set importe = ?, fechaHora = ? where idCompra = ?";
+            ps = cn.prepareStatement(sql);
+            ps.setDouble(1, importe);
+            ps.setString(2, fechaHora);
+            ps.setString(3, idCompra);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+        }finally{
+            cn.close();
+            ps.close();
+        }
+    }
+    
+    public void eliminar(String idCompra) throws SQLException{
+        cn = Conexion.getInstancia().miConexion();
+        PreparedStatement ps = null;
+        try {
+            String sql = "delete from compra where idCompra = ?";
+            ps = cn.prepareStatement(sql);
+            ps.setString(1, idCompra);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+        }finally{
+            cn.close();
+            ps.close();
+        }
+    }
+    
+    public void mostrar(DefaultTableModel modelo) throws SQLException{
+        cn = Conexion.getInstancia().miConexion();
+        PreparedStatement ps = null;
+        String titulos[] = {"ID COMPRA", "IMPORTE", "FECHAHORA"};
+        modelo.getDataVector().removeAllElements();
+        modelo.setColumnIdentifiers(titulos);
+        try {
+            String sql = "select * from compra";
+            ps = cn.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                String idCompra = rs.getString("idCompra");
+                double importe = rs.getDouble("importe");
+                String fechaHora = rs.getString("fechaHora");
+                String fila[] = {idCompra, importe, fechaHora};
+                modelo.addRow(fila);
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+        }finally{
+            cn.close();
+            ps.close();
+        }   
+    }
+    
+    public void mostrarPorId(DefaultTableModel modelo, String id) throws SQLException{
+        cn = Conexion.getInstancia().miConexion();
+        PreparedStatement ps = null;
+        String titulos[] = {"ID COMPRA", "IMPORTE", "FECHAHORA"};
+        modelo.getDataVector().removeAllElements();
+        modelo.setColumnIdentifiers(titulos);
+        
+        try {
+            String sql = "select * from compra where idCompra like ?";
+            ps = cn.prepareStatement(sql);
+            ps.setString(1, id + "%");
+            rs = ps.executeQuery();
+            while(rs.next()){
+                String idCompra = rs.getString("idCompra");
+                double importe = rs.getDouble("importe");
+                String fechaHora = rs.getString("fechaHora");
+                String fila[] = {idCompra, importe, fechaHora};
+                modelo.addRow(fila);
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+        }finally{
+            cn.close();
+            ps.close();
+        }
+    }
+}
---
+++ src/datos/SupervisorDAO.java
@@ -0,0 +1,187 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datos;
+import entidad.Supervisor;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JOptionPane;
+import java.sql.*;
+/**
+ *
+ * @author Usuario
+ */
+public class SupervisorDAO {
+    private Connection cn = null;
+    private ResultSet rs = null;
+    
+    private static SupervisorDAO instancia;
+    
+    public static SupervisorDAO getInstancia(){
+        if(instancia == null)
+            instancia = new SupervisorDAO();
+        return instancia;
+    }
+    public void agregar(Supervisor supervisor) throws SQLException{
+        cn = Conexion.getInstancia().miConexion();
+        PreparedStatement ps = null;
+        
+        try {
+            String idSupervisor = supervisor.getIdSupervisor();
+            String nombre = supervisor.getNombre();
+            String contraseña = supervisor.getContraseña();
+            String sql = "insert into supermercado(idSupervisor, nombre, contraseña) values(?, ?, ?)";
+            ps = cn.prepareStatement(sql);
+            ps.setString(1, idSupervisor);
+            ps.setString(2, nombre);
+            ps.setString(3, contraseña);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+        }finally{
+            cn.close();
+            ps.close();
+        }
+    }
+    
+    public Supervisor buscarSupervisor(String idSupervisor) throws SQLException{
+        cn = Conexion.getInstancia().miConexion();
+        PreparedStatement ps = null;
+        Supervisor supervisor = null;
+        try {
+            String sql = "select * from proveedor where idProveedor = ?";
+            ps = cn.prepareStatement(sql);
+            ps.setString(1, idSupervisor);
+            rs = ps.executeQuery();
+            if(rs.next()){
+                String nombre = rs.getString("nombre");
+                String contraseña = rs.getString("contraseña");
+                supervisor = new Supervisor(idSupervisor, nombre, contraseña);
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+        }finally{
+            cn.close();
+            ps.close();
+        }
+        return supervisor;
+    }
+    
+    public void actualizar(Supervisor supervisor) throws SQLException{
+        cn = Conexion.getInstancia().miConexion();
+        PreparedStatement ps = null;
+        try {
+            String idSupervisor = supervisor.getIdSupervisor();
+            String nombre = supervisor.getNombre();
+            String contraseña = supervisor.getContraseña();
+            String sql = "update supervisor set nombre = ?, contraseña = ? where idSupervisor = ?";
+            ps = cn.prepareStatement(sql);
+            ps.setString(1, nombre);
+            ps.setString(2, contraseña);
+            ps.setString(3, idSupervisor);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+        }finally{
+            cn.close();
+            ps.close();
+        }
+    }
+    
+    public void eliminar(String idSupervisor) throws SQLException{
+        cn = Conexion.getInstancia().miConexion();
+        PreparedStatement ps = null;
+        try {
+            String sql = "delete from supervisor where idSupervisor = ?";
+            ps = cn.prepareStatement(sql);
+            ps.setString(1, idSupervisor);
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+        }finally{
+            cn.close();
+            ps.close();
+        }
+    }
+    
+    public void mostrar(DefaultTableModel modelo) throws SQLException{
+        cn = Conexion.getInstancia().miConexion();
+        PreparedStatement ps = null;
+        String titulos[] = {"ID SUPERVISOR", "NOMBRE", "CONTRASEÑA"};
+        modelo.getDataVector().removeAllElements();
+        modelo.setColumnIdentifiers(titulos);
+        try {
+            String sql = "select * from supervisor";
+            ps = cn.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                String idSupervisor = rs.getString("idSupervisor");
+                String nombre = rs.getString("nombre");
+                String contraseña = rs.getString("contraseña");
+                String fila[] = {idSupervisor, nombre, contraseña};
+                modelo.addRow(fila);
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+        }finally{
+            cn.close();
+            ps.close();
+        }   
+    }
+    
+    public void mostrarPorNombre(DefaultTableModel modelo, String nom) throws SQLException{
+        cn = Conexion.getInstancia().miConexion();
+        PreparedStatement ps = null;
+        String titulos[] = {"ID SUPERVISOR", "NOMBRE", "CONTRASEÑA"};
+        modelo.getDataVector().removeAllElements();
+        modelo.setColumnIdentifiers(titulos);
+        
+        try {
+            String sql = "select * from supervisor where nombre like ?";
+            ps = cn.prepareStatement(sql);
+            ps.setString(1, nom + "%");
+            rs = ps.executeQuery();
+            while(rs.next()){
+                String idSupervisor = rs.getString("idSupervisor");
+                String nombre = rs.getString("nombre");
+                String contraseña = rs.getString("contraseña");
+                String fila[] = {idSupervisor, nombre, contraseña};
+                modelo.addRow(fila);
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+        }finally{
+            cn.close();
+            ps.close();
+        }
+    }
+    
+    public void mostrarPorId(DefaultTableModel modelo, String id) throws SQLException{
+        cn = Conexion.getInstancia().miConexion();
+        PreparedStatement ps = null;
+        String titulos[] = {"ID SUPERVISOR", "NOMBRE", "CONTRASEÑA"};
+        modelo.getDataVector().removeAllElements();
+        modelo.setColumnIdentifiers(titulos);
+        
+        try {
+            String sql = "select * from supervisor where idSupervisor like ?";
+            ps = cn.prepareStatement(sql);
+            ps.setString(1, id + "%");
+            rs = ps.executeQuery();
+            while(rs.next()){
+                String idSupervisor = rs.getString("idSupervisor");
+                String nombre = rs.getString("nombre");
+                String contraseña = rs.getString("contraseña");
+                String fila[] = {idSupervisor, nombre, contraseña};
+                modelo.addRow(fila);
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage(), "ERROR", JOptionPane.ERROR_MESSAGE);
+        }finally{
+            cn.close();
+            ps.close();
+        }
+    }
+}
+    
