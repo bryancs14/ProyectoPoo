442067ecf54eb2cef9bbb15dfa9c2a78ac692527
JampierJRL <janpier.dta@gmail.com>
2021-11-08 04:42:52 -0500

    jampier

--- src/presentacion/DialogCategoria.java
+++ src/presentacion/DialogCategoria.java
@@ -7,6 +7,7 @@ package presentacion;
 
 import datos.CategoriaDAO;
 import entidad.Categoria;
+import java.awt.Color;
 import javax.swing.JOptionPane;
 import java.sql.SQLException;
 import javax.swing.table.DefaultTableModel;
@@ -24,7 +25,7 @@ public class DialogCategoria extends javax.swing.JDialog {
     public DialogCategoria() {
         super(FrmPrincipal.getInstancia(), true);
         initComponents();
-        setSize(600, 505);
+        
         setLocationRelativeTo(null);
         desHabilitar();
         try {
@@ -64,135 +65,162 @@ public class DialogCategoria extends javax.swing.JDialog {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        txtIdCategoria = new javax.swing.JTextField();
+        jPanel1 = new javax.swing.JPanel();
+        jLabel5 = new javax.swing.JLabel();
         txtNombre = new javax.swing.JTextField();
+        jSeparator1 = new javax.swing.JSeparator();
+        jLabel2Direccion = new javax.swing.JLabel();
+        txtIdCategoria = new javax.swing.JTextField();
+        jSeparator2 = new javax.swing.JSeparator();
         btnGuardar = new javax.swing.JButton();
         btnConsultar = new javax.swing.JButton();
         btnActualizar = new javax.swing.JButton();
-        btnEliminar = new javax.swing.JButton();
         btnCancelar = new javax.swing.JButton();
-        btnSalir = new javax.swing.JButton();
+        btnEliminar = new javax.swing.JButton();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        btnSalir = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setUndecorated(true);
+        setResizable(false);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        txtIdCategoria.setBorder(javax.swing.BorderFactory.createTitledBorder("ID Categoria"));
-        getContentPane().add(txtIdCategoria, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 30, 160, 50));
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        txtNombre.setBorder(javax.swing.BorderFactory.createTitledBorder("Nombre"));
-        getContentPane().add(txtNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(210, 30, 340, 50));
+        jLabel5.setFont(new java.awt.Font("Myanmar Text", 1, 18)); // NOI18N
+        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icon/1486506235-groceries-online-shopping-cart-e-commerce-shopping-cart_81445.png"))); // NOI18N
+        jPanel1.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(260, 10, 100, 100));
 
-        btnGuardar.setText("Guardar");
+        txtNombre.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        txtNombre.setForeground(new java.awt.Color(204, 204, 204));
+        txtNombre.setText("Ingrese su nombre");
+        txtNombre.setBorder(null);
+        jPanel1.add(txtNombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 180, 240, 30));
+        jPanel1.add(jSeparator1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 210, 290, 10));
+
+        jLabel2Direccion.setFont(new java.awt.Font("Myanmar Text", 1, 15)); // NOI18N
+        jLabel2Direccion.setText("ID CATEGORIA");
+        jPanel1.add(jLabel2Direccion, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 240, 120, 20));
+
+        txtIdCategoria.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        txtIdCategoria.setForeground(new java.awt.Color(204, 204, 204));
+        txtIdCategoria.setText("Ingrese su IdCategoria");
+        txtIdCategoria.setBorder(null);
+        jPanel1.add(txtIdCategoria, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 270, 240, 30));
+        jPanel1.add(jSeparator2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 300, 290, 10));
+
+        btnGuardar.setBackground(new java.awt.Color(255, 51, 102));
+        btnGuardar.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        btnGuardar.setForeground(new java.awt.Color(51, 51, 51));
+        btnGuardar.setText("GUARDAR");
+        btnGuardar.setBorder(null);
         btnGuardar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnGuardarActionPerformed(evt);
             }
         });
-        getContentPane().add(btnGuardar, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 100, 90, 30));
+        jPanel1.add(btnGuardar, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 340, 90, 40));
 
-        btnConsultar.setText("Consultar");
+        btnConsultar.setBackground(new java.awt.Color(255, 51, 102));
+        btnConsultar.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        btnConsultar.setForeground(new java.awt.Color(51, 51, 51));
+        btnConsultar.setText("CONSULTAR");
+        btnConsultar.setBorder(null);
         btnConsultar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnConsultarActionPerformed(evt);
             }
         });
-        getContentPane().add(btnConsultar, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 100, 90, 30));
+        jPanel1.add(btnConsultar, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 340, 90, 40));
 
-        btnActualizar.setText("Actualizar");
+        btnActualizar.setBackground(new java.awt.Color(255, 51, 102));
+        btnActualizar.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        btnActualizar.setForeground(new java.awt.Color(51, 51, 51));
+        btnActualizar.setText("ACTUALIZAR");
+        btnActualizar.setBorder(null);
         btnActualizar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnActualizarActionPerformed(evt);
             }
         });
-        getContentPane().add(btnActualizar, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 100, 90, 30));
+        jPanel1.add(btnActualizar, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 130, 90, 40));
 
-        btnEliminar.setText("Eliminar");
-        btnEliminar.setToolTipText("");
-        btnEliminar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnEliminarActionPerformed(evt);
-            }
-        });
-        getContentPane().add(btnEliminar, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 150, 90, 30));
-
-        btnCancelar.setText("Cancelar");
+        btnCancelar.setBackground(new java.awt.Color(255, 51, 102));
+        btnCancelar.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        btnCancelar.setForeground(new java.awt.Color(51, 51, 51));
+        btnCancelar.setText("CANCELAR");
+        btnCancelar.setBorder(null);
         btnCancelar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnCancelarActionPerformed(evt);
             }
         });
-        getContentPane().add(btnCancelar, new org.netbeans.lib.awtextra.AbsoluteConstraints(240, 150, 90, 30));
+        jPanel1.add(btnCancelar, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 200, 90, 40));
 
-        btnSalir.setText("Salir");
+        btnEliminar.setBackground(new java.awt.Color(255, 51, 102));
+        btnEliminar.setFont(new java.awt.Font("Tahoma", 1, 12)); // NOI18N
+        btnEliminar.setForeground(new java.awt.Color(51, 51, 51));
+        btnEliminar.setText("ELIMINAR");
+        btnEliminar.setToolTipText("");
+        btnEliminar.setBorder(null);
+        btnEliminar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnEliminarActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnEliminar, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 270, 90, 40));
+
+        jTable1.setBackground(new java.awt.Color(255, 102, 102));
+        jTable1.setModel(modelo);
+        jScrollPane1.setViewportView(jTable1);
+
+        jPanel1.add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 420, 440, 180));
+
+        jLabel3.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icon/categoria2.jpg"))); // NOI18N
+        jPanel1.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(500, -200, 920, 940));
+
+        jLabel6.setFont(new java.awt.Font("Myanmar Text", 1, 15)); // NOI18N
+        jLabel6.setText("NOMBRE");
+        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 150, 90, 20));
+
+        jLabel7.setFont(new java.awt.Font("Myanmar Text", 1, 18)); // NOI18N
+        jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Icon/mouse_pointer_icon_128799.png"))); // NOI18N
+        jLabel7.setText("CATEGORIA");
+        jPanel1.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(110, 40, 170, 50));
+
+        btnSalir.setBackground(new java.awt.Color(255, 255, 255));
+        btnSalir.setFont(new java.awt.Font("Tahoma", 0, 22)); // NOI18N
+        btnSalir.setText("X");
+        btnSalir.setAutoscrolls(true);
+        btnSalir.setBorder(null);
+        btnSalir.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        btnSalir.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btnSalirMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                btnSalirMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                btnSalirMouseExited(evt);
+            }
+        });
         btnSalir.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btnSalirActionPerformed(evt);
             }
         });
-        getContentPane().add(btnSalir, new org.netbeans.lib.awtextra.AbsoluteConstraints(440, 150, 90, 30));
+        jPanel1.add(btnSalir, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 30, 30));
 
-        jTable1.setModel(modelo);
-        jScrollPane1.setViewportView(jTable1);
-
-        getContentPane().add(jScrollPane1, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 200, 520, 220));
+        getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 990, 620));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-
-    private void btnGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGuardarActionPerformed
-        String idCategoria = txtIdCategoria.getText();
-        String nombre = txtNombre.getText();
-
-        Categoria categoria = new Categoria(idCategoria, nombre);
-        try {
-            CategoriaDAO.getInstancia().agregar(categoria);
-            CategoriaDAO.getInstancia().mostrar(modelo);
-            JOptionPane.showMessageDialog(null, "Categoría Registrada");
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, ex.getMessage());
-        }
-        limpiarEntradas();
-        desHabilitar();
-    }//GEN-LAST:event_btnGuardarActionPerformed
-
-    private void btnConsultarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnConsultarActionPerformed
-        String idCategoria = txtIdCategoria.getText();
-        if(idCategoria.equalsIgnoreCase(""))
-        JOptionPane.showMessageDialog(null, "POR FAVOR INGRESA EL ID DE LA CATEGORIA");
-        else
-        {
-            try {
-                Categoria x = CategoriaDAO.getInstancia().buscarCategoria(idCategoria);
-                if(x != null) {
-                    txtIdCategoria.setText(x.getIdCategoria());
-                    txtNombre.setText(x.getNombre());
-                    habilitar();
-                }
-                else
-                JOptionPane.showMessageDialog(null,"El ID no existe");
-            }catch (SQLException ex) {
-                JOptionPane.showMessageDialog(null,ex.getMessage());
-            }
-        }
-    }//GEN-LAST:event_btnConsultarActionPerformed
-
-    private void btnActualizarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnActualizarActionPerformed
-        try {
-            String idCategoria = txtIdCategoria.getText();
-            String nombre = txtNombre.getText();
-
-            Categoria x = new Categoria(idCategoria, nombre);
-            CategoriaDAO.getInstancia().actualizar(x);
-            CategoriaDAO.getInstancia().mostrar(modelo);
-            JOptionPane.showMessageDialog(null,"Dato modificado");
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null,ex.getMessage());
-        }
-        limpiarEntradas();
-        desHabilitar();
-    }//GEN-LAST:event_btnActualizarActionPerformed
 
     private void btnEliminarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnEliminarActionPerformed
         try{
@@ -220,7 +248,78 @@ public class DialogCategoria extends javax.swing.JDialog {
         desHabilitar();
     }//GEN-LAST:event_btnCancelarActionPerformed
 
+    private void btnActualizarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnActualizarActionPerformed
+        try {
+            String idCategoria = txtIdCategoria.getText();
+            String nombre = txtNombre.getText();
+
+            Categoria x = new Categoria(idCategoria, nombre);
+            CategoriaDAO.getInstancia().actualizar(x);
+            CategoriaDAO.getInstancia().mostrar(modelo);
+            JOptionPane.showMessageDialog(null,"Dato modificado");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null,ex.getMessage());
+        }
+        limpiarEntradas();
+        desHabilitar();
+    }//GEN-LAST:event_btnActualizarActionPerformed
+
+    private void btnConsultarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnConsultarActionPerformed
+        String idCategoria = txtIdCategoria.getText();
+        if(idCategoria.equalsIgnoreCase(""))
+        JOptionPane.showMessageDialog(null, "POR FAVOR INGRESA EL ID DE LA CATEGORIA");
+        else
+        {
+            try {
+                Categoria x = CategoriaDAO.getInstancia().buscarCategoria(idCategoria);
+                if(x != null) {
+                    txtIdCategoria.setText(x.getIdCategoria());
+                    txtNombre.setText(x.getNombre());
+                    habilitar();
+                }
+                else
+                JOptionPane.showMessageDialog(null,"El ID no existe");
+            }catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null,ex.getMessage());
+            }
+        }
+    }//GEN-LAST:event_btnConsultarActionPerformed
+
+    private void btnGuardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGuardarActionPerformed
+        String idCategoria = txtIdCategoria.getText();
+        String nombre = txtNombre.getText();
+
+        Categoria categoria = new Categoria(idCategoria, nombre);
+        try {
+            CategoriaDAO.getInstancia().agregar(categoria);
+            CategoriaDAO.getInstancia().mostrar(modelo);
+            JOptionPane.showMessageDialog(null, "Categoría Registrada");
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, ex.getMessage());
+        }
+        limpiarEntradas();
+        desHabilitar();
+    }//GEN-LAST:event_btnGuardarActionPerformed
+
+    private void btnSalirMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnSalirMouseClicked
+        // TODO add your handling code here:
+        System.exit(0);
+    }//GEN-LAST:event_btnSalirMouseClicked
+
+    private void btnSalirMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnSalirMouseEntered
+        // TODO add your handling code here:
+        btnSalir.setBackground(Color.red);
+        btnSalir.setForeground(Color.white);
+    }//GEN-LAST:event_btnSalirMouseEntered
+
+    private void btnSalirMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btnSalirMouseExited
+        // TODO add your handling code here:
+        btnSalir.setBackground(Color.white);
+        btnSalir.setForeground(Color.black);
+    }//GEN-LAST:event_btnSalirMouseExited
+
     private void btnSalirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSalirActionPerformed
+        // TODO add your handling code here:
         dispose();
     }//GEN-LAST:event_btnSalirActionPerformed
 
@@ -274,7 +373,15 @@ public class DialogCategoria extends javax.swing.JDialog {
     private javax.swing.JButton btnEliminar;
     private javax.swing.JButton btnGuardar;
     private javax.swing.JButton btnSalir;
+    private javax.swing.JLabel jLabel2Direccion;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JSeparator jSeparator2;
     private javax.swing.JTable jTable1;
     private javax.swing.JTextField txtIdCategoria;
     private javax.swing.JTextField txtNombre;
